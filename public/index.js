b7482961f51eee87fb3b024e5dce6f4b0a22a5e4
Richard Hildred <rhildred@gmail.com>
2020-03-10 14:55:08 -0400

    got CRD going (no update)

--- public/index.js
+++ public/index.js
@@ -4,9 +4,20 @@ import 'firebase/database';
 
 firebase.initializeApp(config);
 
-function addTodo(sNewTodo){
+firebase.database().ref("todos/").on("value", (snapshot) =>{
+    const oTodos = snapshot.val();
+    const aTodos = Object.keys(oTodos);
+    document.getElementById("todos").innerHTML = "";
+    for(let n = 0; n < aTodos.length; n++){
+        const sTodo = oTodos[aTodos[n]]["name"];
+        addTodo(aTodos[n], sTodo);
+    }
+});
+
+function addTodo(sId, sNewTodo){
     let oParagraph = document.createElement("p");
-    oParagraph.innerHTML = `${sNewTodo}`;
+    oParagraph.innerHTML = `<button id="fin_${sId}" class="finish">&#x2714;</button>
+    <button id="del_${sId}" class="delete">x</button>&nbsp;${sNewTodo}`;
     document.getElementById("todos").prepend(oParagraph);
 }
 
@@ -17,5 +28,22 @@ document.getElementById("todoForm").addEventListener("submit",
     let oNewTodo = document.getElementById("newTodo");
     let sNewTodo = oNewTodo.value;
     oNewTodo.value = "";
-    addTodo(sNewTodo);
+    const sId = new Date().toISOString().replace('.', '_');
+    const sPath = "todos/" + sId;
+    firebase.database().ref(sPath).set({name: sNewTodo});
+});
+
+function hasClass(elem, className) {
+    return elem.classList.contains(className);
+}
+
+document.getElementById("todos").addEventListener("click", 
+(evt)=>{
+    if(hasClass(evt.target, "delete")){
+        //alert("delete button clicked: " + evt.target.id);
+        firebase.database().ref("todos/" + evt.target.id.replace("del_", "")).remove();
+    }
+    else if(hasClass(evt.target, "finish")){
+        alert("finish button clicked: " + evt.target.id);
+    }
 });
